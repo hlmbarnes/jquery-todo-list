7267d897582b6cf95fcea2310ddca7f377b69ee4
Hannah Barnes <hlmbarnes@me.com>
2016-03-29 20:59:48 -0700

    updated the button to generate when the text is submitted in js

--- style.js
+++ style.js
@@ -1,10 +1,20 @@
-$('submit_item').append('<p>').append('stuff goes here');
+
 
 
 $(document).ready(function() {
 	$('#submit_item').on('click', function (){
-		
-		var newItem = ('<li>TEST</li>');
+		event.preventDefault();
+		var text = $('#item_form').val();
+		console.log(text);
+		$newItem = $('<li>' + text + ' <button>Delete</button></li>');
 		$newItem.appendTo('#list');
 		});
 	});
+
+// <li> <input type="radio">
+//         <button>Delete</button> </li>
+
+	// $(this).children('input:text').each(function(index, elem) {
+	// 	var value = $(elem).val()
+	// 	$('body').append('<h2>' + value + '</h2>');
+	// 	$(elem).val('');
