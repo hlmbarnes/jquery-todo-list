f999d5ef2674d951f164ce0176f5195b0dba5b90
Hannah Barnes <hlmbarnes@me.com>
2016-03-29 18:50:29 -0700

    updated JS function to have click event for when you click the submit button as well as updated the div surrounding the list

--- style.js
+++ style.js
@@ -1,1 +1,10 @@
-style.js
+$('submit_item').append('<p>').append('stuff goes here');
+
+
+$(document).ready(function() {
+	$('#submit_item').on('click', function (){
+		
+		var newItem = ('<li>TEST</li>');
+		$newItem.appendTo('#list');
+		});
+	});
